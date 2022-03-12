1a3158ddcc2c5f7becaaabafc4d2f50608fbb4b0
Joshua Yeung <najoshua.yeung@gmail.com>
2022-03-12 15:29:26 +0800

    vim-plug installation in paru.sh

--- paru.sh
+++ paru.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Installing vim-plug for neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # Installing paru
 git clone https://aur.archlinux.org/paru.git
 cd paru
