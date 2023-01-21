088a64eac4eacc414b0ccb218a84db08a11c9cab
Joshua Yeung <najoshua.yeung@gmail.com>
2023-01-21 13:33:48 +0800

    updated suckless and removed neovim

--- aux/suckless/dwmblocks/blocks.h
+++ aux/suckless/dwmblocks/blocks.h
@@ -1,13 +1,13 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{" RAM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"VOL: ", "/home/joshua/scripts/volume.sh",					                            1,		0},
-
-	{"", "date '+%b %d (%a) %H:%M:%S'",				                    	      1,		0},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {" 🐐 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {" ", "acpi -b | awk '{if ($3 == \"Discharging,\") {printf \"🔋 %s %s\", $4, $5} else {printf \"🔌 %s %s\", $4, $5}}'", 30, 0},
+    {" ", "pamixer --get-volume-human | awk '{if ($1 == \"muted\") {printf \"🔇 Muted\"} else {printf \"🔊 %s\", $1}}'", 1, 0},
+    {" 🌞 ", "light", 1, 0},
+    {" 📅 ", "date '+%b %d (%a) %H:%M:%S'", 1, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
+
