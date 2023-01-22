e606c7e7840a4de252e924d6c502661f221e5b6e
Joshua Yeung <najoshua.yeung@gmail.com>
2023-01-22 16:53:43 +0800

    doomemacs

--- aux/suckless/dwmblocks/blocks.h
+++ aux/suckless/dwmblocks/blocks.h
@@ -1,9 +1,9 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     {" 🐐 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
-    {" ", "acpi -b | awk '{if ($3 == \"Discharging,\") {printf \"🔋 %s %s\", $4, $5} else {printf \"🔌 %s %s\", $4, $5}}'", 30, 0},
     {" ", "pamixer --get-volume-human | awk '{if ($1 == \"muted\") {printf \"🔇 Muted\"} else {printf \"🔊 %s\", $1}}'", 1, 0},
-    {" 🌞 ", "light", 1, 0},
+    // {" ", "acpi -b | awk '{if ($3 == \"Discharging,\") {printf \"🔋 %s %s\", $4, $5} else {printf \"🔌 %s %s\", $4, $5}}'", 30, 0},
+    // {" 🌞 ", "light", 1, 0},
     {" 📅 ", "date '+%b %d (%a) %H:%M:%S'", 1, 0},
 };
 
