d44a9880aa0ea931bcb36519ab19e9fb52af6fb8
Joshua Yeung <najoshua.yeung@gmail.com>
2023-02-06 08:27:52 +0800

    x

--- aux/suckless/dwm/config.h
+++ aux/suckless/dwm/config.h
@@ -7,9 +7,9 @@ static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { 
-	"Iosevka Term SS04:style=Light:size=15",
+	"Iosevka Term SS04:style=Light:size=10",
 	"JoyPixels:pixelsize=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Iosevka Term SS04:style=Light:size=15";
+static const char dmenufont[]       = "Iosevka Term SS04:style=Light:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
