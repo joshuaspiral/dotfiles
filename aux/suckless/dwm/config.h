9cc982406becdec981723fa46dc5070dd6ac7863
Joshua Yeung <najoshua.yeung@gmail.com>
2023-10-23 22:35:40 +0800

    fix3

--- aux/suckless/dwm/config.h
+++ aux/suckless/dwm/config.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 15;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:size=15" };
-static const char dmenufont[]       = "Iosevka:size=15";
+static const char *fonts[]          = { "Fira Code Nerd Font:size=15" };
+static const char dmenufont[]       = "Fira Code Nerd Font:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#034efc";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
