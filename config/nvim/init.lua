8a928aa5117047bc0572fa2699ea32c978d07f84
Joshua Yeung <najoshua.yeung@gmail.com>
2026-02-03 03:46:59 -0500

    update: pkglist and kde configs

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,11 +1,11 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
+	vim.fn.system({
+		"git", "clone", "--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -13,53 +13,44 @@ vim.g.mapleader = " "
 
 -- Plugins
 require("lazy").setup({
-  -- Theme
-  { 
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-    config = function() 
-      vim.cmd.colorscheme("catppuccin") 
-    end 
-  },
 
-  -- File Explorer
-  { 
-    "nvim-tree/nvim-tree.lua", 
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function() 
-      require("nvim-tree").setup()
-      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
-    end 
-  },
+	-- File Explorer
+	{ 
+		"nvim-tree/nvim-tree.lua", 
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function() 
+			require("nvim-tree").setup()
+			vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+		end 
+	},
 
-  -- Status Line
-  { 
-    "nvim-lualine/lualine.nvim", 
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function() 
-      require("lualine").setup({ options = { theme = "catppuccin" } }) 
-    end 
-  },
+	-- Status Line
+	{ 
+		"nvim-lualine/lualine.nvim", 
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function() 
+			require("lualine").setup({ options = { theme = "catppuccin" } }) 
+		end 
+	},
 
-  -- Fuzzy Finder
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    end
-  },
+	-- Fuzzy Finder
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+		end
+	},
 
-  -- Git Signs
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require('gitsigns').setup()
-    end
-  }
+	-- Git Signs
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require('gitsigns').setup()
+		end
+	}
 })
 
 -- Basic Settings
